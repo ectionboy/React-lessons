36b41d2a712b261a4c09d686b0f79cedb1c5ceed
ectionboy <125730248+ectionboy@users.noreply.github.com>
2023-10-08 19:38:46 +0300

    move to ContactList dispatch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,27 +2,14 @@ import ContactsList from "./ContactsList/ContactsList";
 import { Filter } from "./Filter/Filter";
 import Input from "./Input/Input";
 
-import {  useDispatch, useSelector } from 'react-redux'
-import { deleteContact } from 'redux/contacts/actions'
-
-
-
 export const App = () => {
-  const { contacts } = useSelector((store) => store.contacts)
-
-  const dispatch = useDispatch();
-
-  const deleteItem = id => {
-    dispatch(deleteContact(id));
-  };
-
   return (
     <div>
       <Input />
       <p></p>
       <Filter />
       <p></p>
-      <ContactsList contacts={contacts} deleteItem={ deleteItem }/>
+      <ContactsList />
     </div>
   );
 };
--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,25 +1,40 @@
-import React from 'react';
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { deleteContact } from 'redux/contacts/actions'
 
-const ContactsList = ({ contacts, deleteItem }) => {
-  console.log('contacts', contacts);
+const ContactsList = () => {
 
+    const { contacts } = useSelector((store) => store.contacts)
 
+const dispatch = useDispatch()
 
-  return (
+    const deleteItem = (id) =>{
+        dispatch(deleteContact(id))
+
+    }
+    const ct = (id) =>{
+        deleteContact(id)
+
+    }
+
+    return (
     <div>
-      <ul>
-        {contacts &&
-          contacts.map(contact => (
-            <li key={contact.id}>
-              <p>
-                {contact.name}: {contact.number}
-              </p>
-              <button onClick={() => deleteItem(contact.id)}>Delete</button>
-            </li>
-          ))}
-      </ul>
+        <ul>
+      {contacts && contacts.map(contact => (
+        <li key={contact.id}>
+          <p>
+            {contact.name}: {contact.number}
+          </p>
+          <button
+            onClick={() => deleteItem(contact.id)}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
     </div>
-  );
-};
+  )
+}
 
-export default ContactsList;
+export default ContactsList
